faf8382a2f4228b6a6f2d33bedf5aa420d829193
benhayehudi <bengreenberg@gmail.com>
2020-04-10 11:33:57 +0300

    persist session_id and refactor logic in backend to use persisted session_id if it exists

--- app/controllers/video_controller.rb
+++ app/controllers/video_controller.rb
@@ -1,19 +1,39 @@
 require 'opentok'
 
 class VideoController < ApplicationController
+  skip_before_action :verify_authenticity_token, if: :json_request?
+
+  def json_request?
+    request.format.json?
+  end
+
+  def landing
+  end
+
+  def name
+    @name = name_params[:name]
+    redirect_to party_url(:name => @name)
+  end
 
   def index
     @opentok = OpenTok::OpenTok.new ENV['OPENTOK_API_KEY'], ENV['OPENTOK_API_SECRET']
     @api_key = ENV['OPENTOK_API_KEY']
     @api_secret = ENV['OPENTOK_API_SECRET']
     session = @opentok.create_session
-    @session_id = session.session_id
-    @token = session.generate_token
+    @session_id = Session.create_or_load_session_id(session.session_id)
+    @token = @opentok.generate_token(@session_id)
+    @name = params[:name]
   end
 
   def chat
   end
 
   def webhook
+  end
+
+  private
+
+  def name_params
+    params.permit(:name, :authenticity_token, :commit)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get '/', to: 'video#index'
+  get '/', to: 'video#landing'
+  get '/party', to: 'video#index'
+  post '/name', to: 'video#name'
   post '/chat/send', to: 'video#chat'
   post '/event', to: 'video#webhook'
 end
