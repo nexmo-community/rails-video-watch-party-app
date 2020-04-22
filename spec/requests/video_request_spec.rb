00042105307ab2939432f9fcc652ef3f03aa06d5
benhayehudi <bengreenberg@gmail.com>
2020-04-22 10:02:50 +0300

    add rspec-rails and add model tests for Session

---
+++ spec/requests/video_request_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "Videos", type: :request do
+
+end
--- test/controllers/video_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class VideoControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
