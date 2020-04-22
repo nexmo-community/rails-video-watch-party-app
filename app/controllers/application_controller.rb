eebb3b99dc3d313af694b55e82f0d6dd951442c4
benhayehudi <bengreenberg@gmail.com>
2020-04-22 10:58:26 +0300

    move yaml load into a constant defined once

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,28 +5,25 @@ class ApplicationController < ActionController::Base
   before_action :set_site_welcome_options
   before_action :set_site_navbar_options
   before_action :set_site_chat_options
+  CONFIG = YAML.load_file("#{Rails.root}/config/site_info.yml")
 
   def set_site_lang_options
-    config = YAML.load_file("#{Rails.root}/config/site_info.yml")
-    @lang = config['language']
-    @lang_dir = config['lang_direction']
+    @lang = CONFIG['language']
+    @lang_dir = CONFIG['lang_direction']
   end
 
   def set_site_welcome_options
-    config = YAML.load_file("#{Rails.root}/config/site_info.yml")
-    @welcome_message = config['landing_page']['welcome_message']['text']
-    @name_form_text = config['landing_page']['name_form']['text']
-    @name_form_submit_button_text = config['landing_page']['name_form']['submit_button_text']
+    @welcome_message = CONFIG['landing_page']['welcome_message']['text']
+    @name_form_text = CONFIG['landing_page']['name_form']['text']
+    @name_form_submit_button_text = CONFIG['landing_page']['name_form']['submit_button_text']
   end
 
   def set_site_navbar_options
-    config = YAML.load_file("#{Rails.root}/config/site_info.yml")
-    @navbar_title = config['navbar']['title']['text']
+    @navbar_title = CONFIG['navbar']['title']['text']
   end
 
   def set_site_chat_options
-    config = YAML.load_file("#{Rails.root}/config/site_info.yml")
-    @submit_button_text = config['text_chat']['submit_button_text']
-    @chat_placeholder_text = config['text_chat']['placeholder_text']
+    @submit_button_text = CONFIG['text_chat']['submit_button_text']
+    @chat_placeholder_text = CONFIG['text_chat']['placeholder_text']
   end
 end
