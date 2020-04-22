c6b545807f0da1cfb5b68c91f4f51e58972b1b40
benhayehudi <bengreenberg@gmail.com>
2020-04-22 10:11:03 +0300

    add request specs for video controller

--- spec/requests/video_request_spec.rb
+++ spec/requests/video_request_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe "Videos", type: :request do
+  context 'when visiting the landing page' do
+    it 'renders the :landing template' do
+      get '/'
+      expect(response).to render_template(:landing)
+    end
+  end
 
+  context 'when visiting the party page' do
+    it 'renders the :index template' do
+      get '/party'
+      expect(response).to render_template(:index)
+    end
+  end
+
+  context 'when visiting the screenshare page' do
+    it 'renders the :screenshare template' do
+      get '/screenshare'
+      expect(response).to render_template(:screenshare)
+    end
+  end
 end
